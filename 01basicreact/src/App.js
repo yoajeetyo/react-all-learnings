133f1bb2dbb3bc8c464be53a7d1cddabdca060c9
yoajeetyo <sajeetkumar40@gmail.com>
2023-11-14 12:18:30 +0530

    folder structure and naming convention

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,24 @@
+// file structures and best practices in react
+
+// most of the times we will create files and write codes inside src folder
+// index.html is present inside public folder
+// we do not needs to invoke scripts using <script> tag inside index.html, it gets automatically invoked because of react-scripts
+        // dependency written inside package.json file
+// we can write components file name starting with small or capital letter but best practice is write in starting letter with capital letter
+// components function name should be starting with capital letter
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <h1>Hello world !!!</h1>
+    <h1>Hello world from react !!!</h1>
   );
 }
 
